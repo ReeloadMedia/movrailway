f3a9b2820a9ccb1922d218b8f6173152ce0dbfb7
ReeloadMedia <90978946+ReeloadMedia@users.noreply.github.com>
2022-03-25 03:31:06 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/ReeloadMedia/movrailway.git /ReeloadMedia
+  git clone https://github.com/ReeloadMedia/movrailway.git /movrailway
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /ReeloadMedia
+  git clone $UPSTREAM_REPO /movrailway
 fi
-cd /ReeloadMedia
+cd /movrailway
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
