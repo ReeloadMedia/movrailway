c19a243fa939781f0bb0b242a9ac5d9d084367c2
ReeloadMedia <90978946+ReeloadMedia@users.noreply.github.com>
2022-03-25 03:37:30 -0400

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -456,7 +456,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
-            text=script.SOURCE_TXT,
+            text=script.BOTS_TXT,
             reply_markup=reply_markup,
             parse_mode='html'
         )
